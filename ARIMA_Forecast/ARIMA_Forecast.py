88488ecff7da40da0a7fd86b7219e1fef13eb9fb
jacowci61 <jacowci61@gmail.com>
2025-03-17 23:12:45 +0300

    Add project files.

---
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -0,0 +1,13 @@
+# -*- coding: cp1251 -*-
+import pandas as pd
+# import statsmodels.api as sm
+from statsmodels.tsa.arima.model import ARIMA
+
+inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src.xlsx')
+inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace = True)
+
+inputTable = inputTable.melt(id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
+inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
+
+print(inputTable.head(50))
+print(inputTable.sort_values(by=['Регион продаж', 'Продукция','Дата продажи']).reset_index(drop = False).head(50))
