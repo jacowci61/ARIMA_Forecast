016bc40fe287eca828d301ef87b9cce577a3e7a4
jacowci61 <jacowci61@gmail.com>
2025-03-17 23:41:15 +0300

    Added grouping And it seemingly fixed sprting issue i had, this time it all works as it was in loginom

--- ARIMA_Forecast/ARIMA_Forecast.py
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -8,6 +8,11 @@ inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace
 
 inputTable = inputTable.melt(id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
 inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
-
 print(inputTable.head(50))
-print(inputTable.sort_values(by=['Регион продаж', 'Продукция','Дата продажи']).reset_index(drop = False).head(50))
+
+# print(inputTable.sort_values(by=['Регион продаж', 'Продукция','Дата продажи']).reset_index(drop = False).head(50))
+
+grouped = inputTable.groupby(['Регион продаж', 'Продукция'])
+for (region, product), group in grouped:
+    print(region + " " + product)
+    print(group)
