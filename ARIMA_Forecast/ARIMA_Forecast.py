fd207df187f00e7d56b4ba73cb40d713d2e34628
jacowci61 <jacowci61@gmail.com>
2025-03-18 19:44:46 +0300

    Kind of broken ARIMA forecast, taken from here: https://stackoverflow.com/questions/62488182/arima-model-with-pandas-dataframe

--- ARIMA_Forecast/ARIMA_Forecast.py
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -1,18 +1,30 @@
 # -*- coding: cp1251 -*-
 import pandas as pd
-# import statsmodels.api as sm
-from statsmodels.tsa.arima.model import ARIMA
+import statsmodels.api as sm
+# from statsmodels.tsa.arima.model import ARIMA
 
-inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src.xlsx')
+# inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src.xlsx')
+inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src_edited.xlsx')
 inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace = True)
 
 inputTable = inputTable.melt(id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
 inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
-print(inputTable.head(50))
+# print(inputTable.head(50))
 
 # print(inputTable.sort_values(by=['Регион продаж', 'Продукция','Дата продажи']).reset_index(drop = False).head(50))
 
 grouped = inputTable.groupby(['Регион продаж', 'Продукция'])
+
 for (region, product), group in grouped:
-    print(region + " " + product)
-    print(group)
+    forecast_dataframe = group[['Дата продажи','Кол-во продаж']].copy()
+    print(forecast_dataframe.index.dtype)
+    print(forecast_dataframe['Дата продажи'].dtype)
+    # forecast_dataframe.index = forecast_dataframe.DatetimeIndex(forecast_dataframe.index).to_period('M')
+    # print(forecast_dataframe.head(50))
+    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
+    model_fit = model.fit()
+    print(model_fit.summary())
+
+# for (region, product), group in grouped:
+#      print(region + " " + product)
+#      print(group)
