56db602a5d3cfb22363ebf9c0f7f87de435fd94f
jacowci61 <jacowci61@gmail.com>
2025-03-19 17:08:35 +0300

    Seemingly working forecast for next month, but: - forecasted value shows above the inital group for some reason (?) - uncofigured output, e.g not formatted and not writing to excel file - didn't check forecasted values with real data

--- ARIMA_Forecast/ARIMA_Forecast.py
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -1,6 +1,8 @@
 # -*- coding: cp1251 -*-
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
+from statsmodels.tsa.vector_ar.vecm import forecast
 # from statsmodels.tsa.arima.model import ARIMA
 
 # inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src.xlsx')
@@ -17,14 +19,25 @@ grouped = inputTable.groupby(['Регион продаж', 'Продукция'])
 
 for (region, product), group in grouped:
     forecast_dataframe = group[['Дата продажи','Кол-во продаж']].copy()
-    print(forecast_dataframe.index.dtype)
-    print(forecast_dataframe['Дата продажи'].dtype)
+    # print(forecast_dataframe)
+    forecast_dataframe['Дата продажи'] = pd.to_datetime(forecast_dataframe['Дата продажи'])
+    forecast_dataframe.set_index('Дата продажи', inplace = True)
+    print(forecast_dataframe)
+    #forecast_data = np.array(forecast_dataframe)
+    # print(forecast_data)
+    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
+    #model_fit = model.fit()
+    print(model.fit().forecast(1))
+    # forecast_dataframe['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
+    # forecast_dataframe = forecast_dataframe.to_numpy()
+    # print(forecast_dataframe.index.dtype)
+    # print(forecast_dataframe['Дата продажи'].dtype)
     # forecast_dataframe.index = forecast_dataframe.DatetimeIndex(forecast_dataframe.index).to_period('M')
     # print(forecast_dataframe.head(50))
-    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
-    model_fit = model.fit()
-    print(model_fit.summary())
+    # print(forecast_dataframe)
+    # model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
+    # model_fit = model.fit()
 
 # for (region, product), group in grouped:
 #      print(region + " " + product)
-#      print(group)
+#      print(group) 
