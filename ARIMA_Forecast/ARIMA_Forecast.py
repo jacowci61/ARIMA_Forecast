f5a3988ec497308fe6b548890bcc0a98b0e6fff8
jacowci61 <jacowci61@gmail.com>
2025-03-20 20:02:15 +0300

    A bit more clean output of predicted values - also removed commented out code

--- ARIMA_Forecast/ARIMA_Forecast.py
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -11,33 +11,19 @@ inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace
 
 inputTable = inputTable.melt(id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
 inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
-# print(inputTable.head(50))
-
-# print(inputTable.sort_values(by=['Регион продаж', 'Продукция','Дата продажи']).reset_index(drop = False).head(50))
 
 grouped = inputTable.groupby(['Регион продаж', 'Продукция'])
+outputlist = []
+groupCounter = 0
 
 for (region, product), group in grouped:
     forecast_dataframe = group[['Дата продажи','Кол-во продаж']].copy()
-    # print(forecast_dataframe)
     forecast_dataframe['Дата продажи'] = pd.to_datetime(forecast_dataframe['Дата продажи'])
     forecast_dataframe.set_index('Дата продажи', inplace = True)
-    print(forecast_dataframe)
-    #forecast_data = np.array(forecast_dataframe)
-    # print(forecast_data)
-    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
-    #model_fit = model.fit()
-    print(model.fit().forecast(1))
-    # forecast_dataframe['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
-    # forecast_dataframe = forecast_dataframe.to_numpy()
-    # print(forecast_dataframe.index.dtype)
-    # print(forecast_dataframe['Дата продажи'].dtype)
-    # forecast_dataframe.index = forecast_dataframe.DatetimeIndex(forecast_dataframe.index).to_period('M')
-    # print(forecast_dataframe.head(50))
-    # print(forecast_dataframe)
-    # model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'], order = (1,1,1))
-    # model_fit = model.fit()
+    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'])
+    outputlist.append(model.fit().forecast(steps = 1))
+    groupCounter += 1
 
-# for (region, product), group in grouped:
-#      print(region + " " + product)
-#      print(group) 
+outputTable = pd.DataFrame(outputlist)
+print(outputTable)
+print(groupCounter)
