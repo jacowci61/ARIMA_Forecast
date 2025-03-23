0cf5dfe17c7ee75ccbbecda6f4ab01e58b7a5d9d
jacowci61 <jacowci61@gmail.com>
2025-03-23 18:25:19 +0300

    Mid-changes backup. Reworking transpositioning of the table

--- ARIMA_Forecast/ARIMA_Forecast.py
+++ ARIMA_Forecast/ARIMA_Forecast.py
@@ -1,4 +1,6 @@
 # -*- coding: cp1251 -*-
+from collections import Counter
+from itertools import combinations
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -7,62 +9,63 @@ from statsmodels.tsa.vector_ar.vecm import forecast
 
 inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src.xlsx')
 # inputTable = pd.read_excel('D:\PocoX3\Work\Involux\Прогнозирование\src_edited.xlsx')
+#inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace = True)
 inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace = True)
-inputTable.drop(inputTable.columns[len(inputTable.columns)-1], axis = 1, inplace = True)
-print(inputTable)
+(inputTable[['Регион продаж','Продукция']].drop_duplicates()).to_excel("combinations_test.xlsx")
+dates = inputTable.columns[2:]
+print(dates[0])
+print(dates[-2])
+# print(inputTable.iloc[1].values(len(inputTable.columns)-1))
 
-inputTable = inputTable.melt(id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
-inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
+# inputTable.to_excel("read_test.xlsx")
+# (inputTable.sort_values(by = 'Регион продаж')).to_excel("read_test.xlsx") sorting not needed
 
-unique_regions = inputTable['Регион продаж'].unique()
-unique_products = inputTable['Продукция'].unique()
-unique_dates = pd.date_range(start=inputTable['Дата продажи'].min(), end=inputTable['Дата продажи'].max(), freq = 'MS')
+# inputTable['Регион продаж'] = pd.Categorical(inputTable['Регион продаж'], categories = inputTable['Регион продаж'].unique(), ordered = True)
+# inputTable['Продукция'] = pd.Categorical(inputTable['Продукция'], categories = inputTable['Продукция'].unique(), ordered = True)
 
-cartesian_inputTable = pd.MultiIndex.from_product([unique_regions, unique_products, unique_dates] ,names = ['Регион продаж', 'Продукция', 'Дата продажи']).to_frame(index = False)
-final_df = cartesian_inputTable.merge(inputTable, on = ['Регион продаж', 'Продукция', 'Дата продажи'], how = 'left')
-print("\n")
-print(pd.DataFrame(final_df))
+#inputTable1 = pd.melt(inputTable, id_vars=['Регион продаж', 'Продукция'], var_name='Дата продажи', value_name='Кол-во продаж')
+#inputTable1.sort_values(['Регион продаж', 'Продукция'], key=lambda x: x.cat.codes)
+#inputTable['Дата продажи'] = pd.to_datetime(inputTable['Дата продажи'], format='%d.%m.%Y')
+#print(inputTable1)
+#print(len(inputTable1))
+#inputTable1.to_excel("transpose_test.xlsx")
 
-print("\n")
-print(pd.DataFrame(inputTable))
 
-grouped = final_df.groupby(['Регион продаж', 'Продукция'])
-print("\n")
-print(pd.DataFrame(grouped))
+# unique_regions = inputTable['Регион продаж'].unique()
+# unique_products = inputTable['Продукция'].unique()
+# print(len(unique_products))
+# print(len(unique_regions))
+# unique_dates = pd.date_range(start=inputTable['Дата продажи'].min(), end=inputTable['Дата продажи'].max(), freq = 'MS')
 
-# outputlist = []
-# groupcounter = 0
+# cartesian_inputTable = pd.MultiIndex.from_product([unique_regions, unique_products, unique_dates] ,names = ['Регион продаж', 'Продукция', 'Дата продажи']).to_frame(index = False)
+# final_df = cartesian_inputTable.merge(inputTable, on = ['Регион продаж', 'Продукция', 'Дата продажи'], how = 'left')
+# print(len(cartesian_inputTable))
+# print("\n")
+# print(pd.DataFrame(final_df))
 
-# for (region, product), group in grouped:k
-#     print(group)
+# print("\n")
+# print(pd.DataFrame(inputTable))
+
+# grouped = final_df.groupby(['Регион продаж', 'Продукция'])
+# print("\n")
+# print(pd.DataFrame(grouped))
+
+# outputlist = pd.DataFrame(columns = ['Регион продаж', 'Продукция', 'Дата продажи', 'Кол-во продаж'])
+# groupCounter = -1
+
+# for (region, product), group in grouped:
+#     groupCounter += 1 #5287 groups in summary
 #     forecast_dataframe = group[['Дата продажи','Кол-во продаж']].copy()
 #     forecast_dataframe['Дата продажи'] = pd.to_datetime(forecast_dataframe['Дата продажи'])
 #     forecast_dataframe.set_index('Дата продажи', inplace = True)
 #     model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'])
-#     print("\n")
-#     print("Region: ")
-#     print(region)
-#     print("\n")
-#     print("Product ID: ")
-#     print(product)
-#     print("Forecasted value: ")
-#     print(str(model.fit().forecast(steps = 1)).split(" "))
-#     # print(model.fit().forecast(steps = 1))
-#     outputlist.append(str(model.fit().forecast(steps = 1)).split(" ")) # need to use dataframe here w/ 3 columns
+#     # outputlist.append(str(model.fit().forecast(steps = 1)).split(" "))
+#     outputlist.loc[groupCounter] = [str(region), str(product), str(model.fit().forecast(steps = 1)).split(" ")[0], str(model.fit().forecast(steps = 1)).split(" ")[4].replace('\nFreq:', '')]
 
-outputlist = pd.DataFrame(columns = ['Регион продаж', 'Продукция', 'Дата продажи', 'Кол-во продаж'])
-groupCounter = -1
+# outputlist.to_excel("output.xlsx")
+# print(groupCounter)
 
-for (region, product), group in grouped:
-    groupCounter += 1
-    forecast_dataframe = group[['Дата продажи','Кол-во продаж']].copy()
-    forecast_dataframe['Дата продажи'] = pd.to_datetime(forecast_dataframe['Дата продажи'])
-    forecast_dataframe.set_index('Дата продажи', inplace = True)
-    model = sm.tsa.arima.ARIMA(forecast_dataframe['Кол-во продаж'])
-    # outputlist.append(str(model.fit().forecast(steps = 1)).split(" "))
-    outputlist.loc[groupCounter] = [str(region), str(product), str(model.fit().forecast(steps = 1)).split(" ")[0], str(model.fit().forecast(steps = 1)).split(" ")[4].replace('\nFreq:', '')]
 
-outputlist.to_excel("output.xlsx")
 # fix this ---------------
     # issue is in getting unique forecasted values to see if there are duplicates, because now amount of forecasted values and
     # amount of real values doesnt match
